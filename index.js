ebe207a14d94d5b7b0e4187ea81e8ab25d496926
jeevan <jeevanmalavi07@gmail.com>
2022-02-18 17:18:13 +0530

    sss

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require("express");
 var bodyparser = require("body-parser");
-var cookie = require ("cookie-parser");
+var cookie = require("cookie-parser");
 var multer = require("multer");
 
 var mongoose = require("mongoose");
-
-
-
+//fgfdgfd
 
 
 var app = express();
@@ -15,8 +13,8 @@ var app = express();
 
 mongoose.connect("mongodb://localhost:27017/e-doc-node");
 const db = mongoose.connection;
-db.on("error",error => console.log(error));
-db.on("open",()=> console.log("connection successfull"));
+db.on("error", error => console.log(error));
+db.on("open", () => console.log("connection successfull"));
 
 
 
@@ -24,11 +22,10 @@ db.on("open",()=> console.log("connection successfull"));
 app.use(bodyparser.json());
 
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
-    if(req.method == "OPTIONS")
-    {
+    if (req.method == "OPTIONS") {
         res.header("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE");
         return res.status(200).json({});
     }
@@ -36,17 +33,17 @@ app.use((req, res, next)=>{
 });
 
 
-app.use("/authentication" ,require("./routes/authentication"));
-app.use("/documents" ,require("./routes/documents"));
-app.use("/words" ,require("./routes/words"));
-app.use("/employees" ,require("./routes/employees"));
+app.use("/authentication", require("./routes/authentication"));
+app.use("/documents", require("./routes/documents"));
+app.use("/words", require("./routes/words"));
+app.use("/employees", require("./routes/employees"));
 
-app.listen(8081 , function (){
+app.listen(8081, function() {
     console.log("Edoc project");
 });
 
 
-app.get("/" , function(req,res){
+app.get("/", function(req, res) {
     res.send("welcome to iGap")
 
 })
